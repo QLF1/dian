4988b66c8203a70ef8a5f0ef176fcfba6f7037b1
qlf <2218721937@qq.com>
2018-07-14 02:50:31 +0800

    correct code

--- task2.sh
+++ task2.sh
@@ -3,19 +3,19 @@
 #自动根据文件后缀名解压缩
 
 filename=$1                    #获取压缩文件名
-if test $filename -eq '--list'
+if [ $filename = '--list' ];
     then
-    echo"supported file types: zip tar tar.gz tar.bz2"
-else if
+    echo "file types: zip tar tar.gz tar.bz2";
+else
     houzhui="${filename##*.}"
     case $houzhui in
 	'zip')
-	    unzip $filename  
+	    unzip $filename ;;
 	'tar')
-	    tar -xf $filename  
+ 	    tar -xf $filename;;
 	'tar.gz')
-	    tar -zxf $filename  
+	    tar -zxf $filename;;  
 	'tar.bz2')
-	    tar tar -xjf $filename  
+	    tar tar -xjf $filename;;  
 	esac
 fi
